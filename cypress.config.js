c0599052f3b96a157c4de4aecaa75e5bbd1f17af
Setup boilerplate <setup-boilerplate@example.com>
2023-09-25 11:50:43 +0000

    Initialize boilerplate frontend-boilerplate-react-nextjs

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   env: {
-    apiUrl: 'http://host.docker.internal:8080',
+    apiUrl: 'null',
   },
   e2e: {
     setupNodeEvents(on, config) {},
